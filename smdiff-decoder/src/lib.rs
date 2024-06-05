fe57a5113037a56e10fd63fa25fbbecf1e41e878
Al Dente <code.alt@8shield.net>
2024-06-05 15:05:20 -0600

    docs and adjustments

--- smdiff-decoder/src/lib.rs
+++ smdiff-decoder/src/lib.rs
@@ -1,59 +1,64 @@
-use std::{fmt::Debug, io::{Read, Seek, Write}};
+use std::io::{Read, Seek, Write};
 
-use reader::SectionReader;
-use smdiff_common::SectionHeader;
+use smdiff_common::MAX_INST_SIZE;
 
 pub mod zstd{
+    //! Re-exports the zstd streaming decoder used
     pub use ruzstd::streaming_decoder::StreamingDecoder;
 }
 pub mod brotli{
+    //! Re-exports the brotli decompressor reader used
     pub use brotlic::DecompressorReader;
 }
 pub mod reader;
-///Applies a SMDiff patch to a source buffer
+///Applies an SMDiff patch to a source buffer
 /// # Arguments
 /// * `patch` - A Read object that contains the SMDiff patch data
 /// * `src` - An optional mutable reference to a Read+Seek object that contains the source (dictionary) data
 /// * `sink` - A Write object that will receive the patched data
 /// # Errors
 /// Returns an error if there is an issue reading from the patch or source data, or writing to the sink
-pub fn apply_patch<P:Read+Seek+Debug,R:Read+Seek,W:Write>(patch:&mut P,mut src:Option<&mut R>,sink:&mut W) -> std::io::Result<()> {
-    //to avoid the Read+Seek bound on sink,
-    //we need to scan the whole patch file so we can cache the TargetSourced windows
-    let mut cur_o = Vec::new();
+///
+/// Note: To enable patch application to large files, we require Read+Seek on the Sink to handle CopySrc::Output operations
+pub fn apply_patch<P:Read+Seek,R:Read+Seek,W:Write+Read+Seek>(patch:&mut P,mut src:Option<&mut R>,sink:&mut W) -> std::io::Result<()> {
     let mut cur_o_pos: usize = 0;
     //let mut stats = Stats::default();
-    let mut reader = SectionReader::new(patch);
-    while let Some(res) = reader.next(){
-        let (ops,header) = res?;
-        output_ops_no_comp(ops, &mut src, &header,&mut cur_o, &mut cur_o_pos)?;
+    let mut reader = crate::reader::SectionIterator::new(patch);
+    while let Some(res) = reader.next_borrowed(){
+        let (ops,_header) = res?;
+        apply_ops(ops, &mut src, sink, &mut cur_o_pos)?;
     }
-    sink.write_all(&cur_o)?;
     Ok(())
 }
 
-fn apply_no_sec_comp<P:Read,R:Read+Seek,W:Write>(patch:&mut P,mut src:Option<&mut R>,sink:&mut W) -> std::io::Result<()> {
-    let mut cur_o = Vec::new();
+
+fn apply_no_sec_comp<P:Read,R:Read+Seek,W:Write+Read+Seek>(patch:&mut P,mut src:Option<&mut R>,sink:&mut W) -> std::io::Result<()> {
+    //To avoid Seek on write, we must write all the output data to a Vec<u8> first
     let mut cur_o_pos = 0;
-    let mut reader = smdiff_reader::SectionReader::new(patch);
-    while let Some(res) = reader.next(){
-        let (ops,header) = res?;
-        output_ops_no_comp(ops, &mut src, &header,&mut cur_o, &mut cur_o_pos)?;
+    let mut reader = smdiff_reader::SectionIterator::new(patch);
+    while let Some(res) = reader.next_borrowed(){
+        let (ops,_header) = res?;
+        apply_ops(ops, &mut src, sink, &mut cur_o_pos)?;
 
     }
-    sink.write_all(&cur_o)?;
     Ok(())
 }
 
-fn output_ops_no_comp<R:Read+Seek>(ops:&[smdiff_reader::Op],src:&mut Option<&mut R>,header:&SectionHeader,cur_o:&mut Vec<u8>, cur_o_pos: &mut usize) -> std::io::Result<()> {
+/// Applies a series of operations to a buffer
+/// Here `cur_o` represents the output buffer.
+/// We could replace it with W:Write+Read+Seek if we didn't want to allocate the entire output buffer in memory
+/// So... maybe TODO?
+fn apply_ops<R:Read+Seek,W:Write+Read+Seek>(ops:&[smdiff_reader::Op],src:&mut Option<&mut R>,cur_o:&mut W, cur_o_pos: &mut usize) -> std::io::Result<()> {
     //let mut stats = Stats::default();
-    let out_size = header.output_size as usize;
-    cur_o.reserve_exact(out_size);
-    cur_o.resize(cur_o.len() + out_size, 0);
+    //let out_size = header.output_size as usize;
+    cur_o.seek(std::io::SeekFrom::Start(*cur_o_pos as u64))?;
+    // cur_o.reserve_exact(out_size);
+    // cur_o.resize(cur_o.len() + out_size, 0);
+    let mut copy_buffer = vec![0u8;MAX_INST_SIZE];
     for op in ops {
         match op {
             smdiff_common::Op::Add(add) => {
-                cur_o[*cur_o_pos..*cur_o_pos+add.bytes.len()].copy_from_slice(&add.bytes);
+                cur_o.write_all(&add.bytes)?;
                 *cur_o_pos += add.bytes.len();
                 //stats.add();
             },
@@ -65,22 +70,29 @@ fn output_ops_no_comp<R:Read+Seek>(ops:&[smdiff_reader::Op],src:&mut Option<&mut
                             None => panic!("Copy operation without source data"),
                         };
                         src.seek(std::io::SeekFrom::Start(copy.addr))?;
-                        let start_pos = *cur_o_pos;
-                        src.read_exact(&mut cur_o[start_pos..start_pos+copy.len as usize])?;
-                        *cur_o_pos += copy.len as usize;
+                        let len = copy.len as usize;
+                        src.read_exact(&mut copy_buffer[..len])?;
+                        cur_o.write_all(&copy_buffer[..len])?;
+                        *cur_o_pos += len;
                     },
                     smdiff_common::CopySrc::Output => {
                         let start_pos = *cur_o_pos;
-                        cur_o.copy_within(copy.addr as usize..copy.addr as usize+copy.len as usize,start_pos);
-                        *cur_o_pos += copy.len as usize;
+                        cur_o.seek(std::io::SeekFrom::Start(copy.addr as u64))?;
+                        let len = copy.len as usize;
+                        cur_o.read_exact(&mut copy_buffer[..len])?;
+                        cur_o.seek(std::io::SeekFrom::Start(start_pos as u64))?;
+                        cur_o.write_all(&copy_buffer[..len])?;
+                        *cur_o_pos += len;
                     },
                 }
                 //stats.copy();
             },
             smdiff_common::Op::Run(run) => {
                 //stats.run();
-                cur_o[*cur_o_pos..*cur_o_pos+run.len as usize].fill(run.byte);
-                *cur_o_pos += run.len as usize;
+                let len = run.len as usize;
+                copy_buffer[..len].fill(run.byte);
+                cur_o.write_all(&copy_buffer[..len])?;
+                *cur_o_pos += len;
             },
         }
     }
@@ -113,9 +125,9 @@ mod tests {
             0, //addr ivar int 0
         ];
         let mut patch = Cursor::new(patch);
-        let mut sink = Vec::new();
+        let mut sink = Cursor::new(Vec::new());
         apply_patch(&mut patch,Some(&mut src),&mut sink).unwrap();
-        assert_eq!(sink, "Hello! Hello!".as_bytes());
+        assert_eq!(sink.into_inner(), "Hello! Hello!".as_bytes());
     }
     #[test]
     fn test_complex_apply(){
@@ -154,9 +166,9 @@ mod tests {
             0, //addr ivar int 0
         ];
         let mut patch = Cursor::new(patch);
-        let mut sink = Vec::new();
+        let mut sink = Cursor::new(Vec::new());
         apply_patch(&mut patch,Some(&mut src),&mut sink).unwrap();
-        assert_eq!(sink, "Hello! Hello!".as_bytes());
+        assert_eq!(sink.into_inner(), "Hello! Hello!".as_bytes());
     }
 
     #[test]
@@ -210,67 +222,9 @@ mod tests {
             46, //'.'
         ];
         let mut patch = Cursor::new(patch);
-        let mut sink = Vec::new();
+        let mut sink = Cursor::new(Vec::new());
         apply_patch(&mut patch,Some(&mut src),&mut sink).unwrap();
-        assert_eq!(sink, "Hello! Hello! Hell...".as_bytes());
+        assert_eq!(sink.into_inner(), "Hello! Hello! Hell...".as_bytes());
 
     }
-
-    // #[test]
-    // fn test_kitchen_sink2(){
-    //     // "hello world!" -> "Hello! Hello! Hello. "
-    //     let mut src = Cursor::new("hello world!".as_bytes().to_vec());
-
-    //     //from encoder tests
-    //     let patch = vec![
-    //         192, // 0b1_1_000_000
-    //         1, //Num ops uvarint
-    //         1, //Num add bytes uvarint
-    //         0, //Output size uvarint diff encoded from add uvarint
-    //         129, //ADD, Size 1 0b10_000001
-    //         72, //'H'
-
-    //         192, // 0b1_1_000_000
-    //         1, //Num ops uvarint
-    //         0, //Num add bytes uvarint
-    //         4, //Output size uvarint diff encoded from add uvarint
-    //         4, //COPY_D, Size 4 0b00_000100
-    //         2, //addr ivar int +1
-
-    //         192, // 0b1_1_000_000
-    //         1, //Num ops uvarint
-    //         2, //Num add bytes uvarint
-    //         0, //Output size uvarint diff encoded from add uvarint
-    //         130, //ADD, Size 2 0b10_000010
-    //         33, //'!'
-    //         32, //' '
-
-    //         192, // 0b1_1_000_000
-    //         1, //Num ops uvarint
-    //         0, //Num add bytes uvarint
-    //         6, //Output size uvarint diff encoded from add uvarint
-    //         70, //COPY_O, Size 6 0b01_000110
-    //         0, //addr ivar int 0
-
-    //         192, // 0b1_1_000_000
-    //         1, //Num ops uvarint
-    //         0, //Num add bytes uvarint
-    //         4, //Output size uvarint diff encoded from add uvarint
-    //         68, //COPY_O, Size 4 0b01_000100
-    //         12, //addr ivar int +6
-
-    //         64, // 0b0_1_000_000
-    //         1, //Num ops uvarint
-    //         0, //Num add bytes uvarint
-    //         3, //Output size uvarint diff encoded from add uvarint
-    //         195, //Run, Size 3 0b11_000011
-    //         46, //'.'
-    //     ];
-    //     let mut patch = Cursor::new(patch);
-    //     let mut sink = Vec::new();
-    //     apply_patch(&mut patch,Some(&mut src),&mut sink).unwrap();
-    //     let str = std::str::from_utf8(&sink).unwrap();
-    //     assert_eq!(str, "Hello! Hello! Hello. ");
-
-    // }
 }
--- smdiff-decoder/src/reader.rs
+++ smdiff-decoder/src/reader.rs
@@ -1,16 +1,20 @@
-use std::io::{BufReader, Read, Seek};
+//! This module contains the `SectionReader` struct, which is used to read sections from a smdiff delta file.
+//! This is just like the `SectionIterator` struct from the `smdiff-reader` crate, but this can read sections that have secondary compression.
+use std::io::{BufReader, Cursor, Read, Seek};
 
 use smdiff_common::SectionHeader;
 use smdiff_reader::{read_ops_no_comp, read_section_header, Op};
 
 use crate::apply_no_sec_comp;
-pub struct SectionReader<R>{
+
+/// A reader that will keep reading sections until it reaches the terminal section.
+pub struct SectionIterator<R>{
     source: BufReader<R>,
     done:bool,
     win_data: Vec<u8>,
     ops: Vec<Op>,
 }
-impl<R: Read+Seek> SectionReader<R>{
+impl<R: Read+Seek> SectionIterator<R>{
     pub fn new(patch: R) -> Self {
         Self {
             source:BufReader::new(patch),
@@ -19,50 +23,79 @@ impl<R: Read+Seek> SectionReader<R>{
             ops: Vec::new(),
         }
     }
-    pub fn next(&mut self) -> Option<std::io::Result<(&[Op],SectionHeader)>>{
-        if self.done{
-            return None;
-        }
-        self.win_data.clear();
-        self.ops.clear();
-        let header = match read_section_header(&mut self.source){
+    /// Reads and returns the next section (if it exists).
+    ///
+    /// This is useful if you don't need the Ops, just need to read them.
+    pub fn next_borrowed(&mut self) -> Option<std::io::Result<(&[Op],SectionHeader)>>{
+        let mut header = match self.read_header()?{
             Ok(h) => h,
             Err(e) => {
                 return Some(Err(e));
             }
         };
+        self.ops.clear();
+        if let Err(e) = self.read_ops(&mut header){
+            return Some(Err(e));
+        }
+        Some(Ok((&self.ops,header)))
+    }
+    ///In the event the caller needs to do something to the ops (more than just read them), this avoids the need to clone the slice.
+    fn next_owned(&mut self) -> Option<std::io::Result<(Vec<Op>,SectionHeader)>>{
+        let mut header = match self.read_header()?{
+            Ok(h) => h,
+            Err(e) => {
+                return Some(Err(e));
+            }
+        };
+        self.ops.clear();
+        if let Err(e) = self.read_ops(&mut header){
+            return Some(Err(e));
+        }
+        Some(Ok((std::mem::take(&mut self.ops),header)))
+    }
+    fn read_header(&mut self) -> Option<std::io::Result<SectionHeader>>{
+        if self.done{
+            return None;
+        }
+        match read_section_header(&mut self.source){
+            Ok(h) => Some(Ok(h)),
+            Err(e) => {
+                return Some(Err(e));
+            }
+        }
+    }
+    fn read_ops(&mut self,header:&mut SectionHeader) -> std::io::Result<()>{
+        self.win_data.clear();
+        self.ops.clear();
         if header.compression_algo == 1 {
-            if let Err(e) = apply_no_sec_comp::<_,R,_>(&mut self.source, None, &mut self.win_data){
-                return Some(Err(e));
-            }
-            let mut crsr = std::io::Cursor::new(&self.win_data);
-            if let Err(e) = read_ops_no_comp(&mut crsr, &header,&mut self.ops){
-                return Some(Err(e));
-            }
+            let mut crsr = Cursor::new(&mut self.win_data);
+            apply_no_sec_comp::<_,R,_>(&mut self.source, None, &mut crsr)?;
+            crsr.rewind()?;
+            read_ops_no_comp(&mut crsr, header,&mut self.ops)?;
         }else if header.compression_algo == 2 {
             let mut zstd = ruzstd::StreamingDecoder::new(&mut self.source).unwrap();
-            if let Err(e) = read_ops_no_comp(&mut zstd, &header,&mut self.ops){
-                return Some(Err(e));
-            }
+            read_ops_no_comp(&mut zstd, header,&mut self.ops)?;
         }else if header.compression_algo == 3 {
             let mut brot = brotlic::DecompressorReader::new(&mut self.source);
-            if let Err(e) = read_ops_no_comp(&mut brot, &header,&mut self.ops){
-                return Some(Err(e));
-            }
-            if let Err(e) = brot.into_inner(){
-                return Some(Err(e.into_error()));
-            }
+            read_ops_no_comp(&mut brot, header,&mut self.ops)?;
+            let _ = brot.into_inner()?;
         }else{
-            if let Err(e) = read_ops_no_comp(&mut self.source, &header,&mut self.ops){
-                return Some(Err(e));
-            }
+            read_ops_no_comp(&mut self.source, header,&mut self.ops)?;
         }
         if !header.more_sections{
             self.done = true;
         }
-        Some(Ok((self.ops.as_slice(),header)))
+        Ok(())
     }
     pub fn into_inner(self) -> R {
         self.source.into_inner()
+    }
+}
+
+impl<R: Read+Seek> Iterator for SectionIterator<R> {
+    type Item = std::io::Result<(Vec<Op>, SectionHeader)>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next_owned()
     }
 }
